868f804f9a361955b3ea4c99dd3c7d042f6211e1
Gabriel Moreira <gabriel.antonioms@gmail.com>
2024-01-28 18:15:09 -0300

    add get rooms com param

--- backend/fakeapi/start.py
+++ backend/fakeapi/start.py
@@ -11,8 +11,8 @@ df = pd.read_csv(variables.CSV_AIRBNB_API)
 df["indice"] = range(1, len(df) + 1)
 df.set_index("indice", inplace=True)
 
-@app.get("/rooms")
-async def get_rooms():
+@app.get("/room")
+async def get_room():
     index = random.randint(1, len(df) - 1)
     tuple = df.iloc[index]  
     fake_data = {
@@ -30,4 +30,31 @@ async def get_rooms():
         "beds": int(tuple["beds"]),
         "baths": int(tuple["baths"])
         }
+    return fake_data
+
+@app.get("/rooms/{num_rooms}")
+async def get_rooms(num_rooms: int):
+    if num_rooms < 1:
+        return "Number of rooms must be greater than 0"
+    
+    fake_data = []
+    for index in range(num_rooms):
+        index = random.randint(1, len(df) - 1)
+        tuple = df.iloc[index]
+        fake_data.append({
+            "name": tuple["name"],
+            "city": "New York",
+            "neighborhood_group": tuple["neighbourhood_group"],
+            "neighborhood": tuple["neighbourhood"],
+            "address": fake.address().split("\n")[0],
+            "room_type": tuple["room_type"],
+            "price": tuple["price"],
+            "number_of_reviews": int(tuple["number_of_reviews"]),
+            "last_review": tuple["last_review"],
+            "license": tuple["license"],
+            "rating": tuple["rating"],
+            "beds": str(tuple["beds"]),
+            "baths": str(tuple["baths"])
+            })
+        
     return fake_data
