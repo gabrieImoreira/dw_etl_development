5820be803ca81c8977766c9462ca797679bdea88
Gabriel Moreira <gabriel.antonioms@gmail.com>
2024-02-12 17:11:59 -0300

    add schedule da aplicacao

--- start.py
+++ start.py
@@ -2,10 +2,19 @@ import variables.variables as variables
 from backend.datasource.API import APICollector
 from backend.contracts.schema import RoomsSchema
 from backend.libraries.AWS.S3Client import S3Client
-import json
+import json, schedule, time
 
 s3 = S3Client()
-api = APICollector(variables.URL_AIRBNB_API, RoomsSchema, s3)
-response = api.start(100)
-response = json.dumps(response, indent=2)
-print(response)
+
+def apiCollector(url_api, schema, aws, repeat):
+    api = APICollector(url_api, schema, aws).start(repeat)
+    # response = json.dumps(response, indent=2)
+    print(f'API Collector started, response: {api}')
+    return
+
+schedule.every(30).seconds.do(apiCollector, variables.URL_AIRBNB_API, RoomsSchema, s3, 50)
+
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
